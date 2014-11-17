a6da17988f414fd8f94d72c4cc5c0f28e19ad816
ryosuke <tmcit.miyazaki@gmail.com>
2014-11-17 11:31:08 +0900

    minor modfied

--- knock030.py
+++ knock030.py
@@ -4,6 +4,9 @@ import sys
 from stemming.porter2 import stem
 
 for line in open(sys.argv[1]):
-	line = line.strip()
-	if line is not "":
-		print line+"\t"+stem(line.split("\t")[1])
+    line = line.strip()
+    if line is not "":
+        print line + "\t" + stem(line.split("\t")[1])
+    else:
+        print
+
